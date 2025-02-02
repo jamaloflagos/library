6ff0d6494b3a50039e19efb0075d680d50bd9031
jamaloflagos <toyinjamal@gmail.com>
2025-02-03 00:21:20 +0100

    Refactor

--- backend/library/asgi.py
+++ backend/library/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for api project.
+ASGI config for library project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,11 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
-django_asgi_app = get_asgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library.settings')
 
-handler = {
-        "http": django_asgi_app,
-    }
+app = get_asgi_application()
